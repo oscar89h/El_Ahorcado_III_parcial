d6c9107f65f89f6cbfa61e97e4c53590ff6622eb
Oscar desde gmail <omarsig420@gmail.com>
2024-11-26 08:41:25 -0600

    agregue una clase para crear la ventana principal

---
+++ VentanaPrincipal.java
@@ -0,0 +1,6 @@
+public class VentanaPrincipal {
+      
+      frame.add(panelDeFigura, BorderLayout.WEST);
+      frame.add(mensaje, BorderLayout.SOUTH);
+      frame.add(panelPalabraOcultaJpanel, BorderLayout.CENTER);
+}
